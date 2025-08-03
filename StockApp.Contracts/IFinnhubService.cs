7d1b471c57c175989e56673897bb08e151967910
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2025-08-02 19:03:45 -0600

    Adding new projects for the solution.

---
+++ StockApp.Contracts/IFinnhubService.cs
@@ -0,0 +1,8 @@
+﻿namespace StockApp.Contracts
+{
+    public abstract class IFinnhubService
+    {
+        public abstract Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol);
+        public abstract Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
+    }
+}
---
+++ StockApp.Services/FinnhubService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using StockApp.Contracts;
+using System.Net.Http;
+
+namespace StockApp.Services
+{
+    public class FinnhubService : IFinnhubService
+    {
+        private readonly HttpClient _httpClient;
+        //private readonly IOptions<>
+
+        public FinnhubService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public override Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
