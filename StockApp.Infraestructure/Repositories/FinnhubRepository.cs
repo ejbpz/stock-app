b05c95ea730c987dacd3513e1c59cabbe967e3b9
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2025-11-28 18:31:54 -0600

    Replace old project with new Clean Architecture.

---
+++ StockApp.Infraestructure/Repositories/FinnhubRepository.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Options;
+using StockApp.Models.Options;
+using StockApp.RepositoryContracts;
+
+namespace StockApp.Repositories
+{
+    public class FinnhubRepository : IFinnhubRepository
+    {
+        private readonly HttpClient _httpClient;
+        private readonly TradingOptions _tradingOptions;
+
+        public FinnhubRepository(IHttpClientFactory httpClientFactory, IOptions<TradingOptions> options)
+        {
+            _tradingOptions = options.Value;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_tradingOptions.FinnhubConnection!}/stock/profile2?symbol={stockSymbol}&token={_tradingOptions.ApiKey!}"),
+            };
+
+            HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage);
+
+            Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            StreamReader reader = new StreamReader(stream);
+            string jsonResponse = await reader.ReadToEndAsync();
+            
+            return JsonSerializer.Deserialize<Dictionary<string, object>?>(jsonResponse);
+        }
+
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_tradingOptions.FinnhubConnection!}/quote?symbol={stockSymbol}&token={_tradingOptions.ApiKey!}")
+            };
+
+            HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage);
+
+            Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            StreamReader reader = new StreamReader(stream);
+            string jsonResponse = await reader.ReadToEndAsync();
+
+            return JsonSerializer.Deserialize<Dictionary<string, object>?>(jsonResponse);
+        }
+
+        public async Task<List<Dictionary<string, string>>?> GetStocks()
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_tradingOptions.FinnhubConnection!}/stock/symbol?exchange=US&token={_tradingOptions.ApiKey!}")
+            };
+
+            HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage);
+            
+            Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            StreamReader reader = new StreamReader(stream);
+            string jsonResponse = await reader.ReadToEndAsync();
+
+            return JsonSerializer.Deserialize<List<Dictionary<string, string>>?>(jsonResponse);
+        }
+
+        public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_tradingOptions.FinnhubConnection!}/search?q={stockSymbolToSearch}&token={_tradingOptions.ApiKey!}")
+            };
+
+            HttpResponseMessage responseMessage = await _httpClient.SendAsync(requestMessage);
+
+            Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            StreamReader reader = new StreamReader(stream);
+            string jsonResponse = await reader.ReadToEndAsync();
+
+            return JsonSerializer.Deserialize<Dictionary<string, object>?>(jsonResponse);
+        }
+    }
+}
---
+++ StockApp.UI/Controllers/StocksController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StockApp.Contracts;
+using StockApp.ViewModels;
+using StockApp.Models.Options;
+
+namespace StockApp.Controllers
+{
+    [Controller]
+    [Route("stocks")]
+    public class StocksController : Controller
+    {
+        private readonly TradingOptions _tradingOptions;
+        private readonly IFinnhubGetterService _finnhubGetterService;
+        private readonly ILogger<TradeController> _logger;
+
+        public StocksController(IOptions<TradingOptions> configureOptions, IFinnhubGetterService finnhubGetterService, ILogger<TradeController> logger)
+        {
+            _tradingOptions = configureOptions.Value;
+            _finnhubGetterService = finnhubGetterService;
+            _logger = logger;
+        }
+
+        [HttpGet("explore")]
+        public async Task<IActionResult> Explore()
+        {
+            _logger.LogInformation("User into ExploreAction in StocksController");
+
+            List<Dictionary<string, string>>? stocksResponse = await _finnhubGetterService.GetStocks();
+            List<Stock> stocks = new List<Stock>();
+
+            ViewBag.SelectedStock = _tradingOptions.DefaultStockSymbol;
+
+            if(stocksResponse is not null)
+            {
+                _logger.LogInformation("Getting the popular stocks");
+                List<string> symbols = _tradingOptions.Top25PopularStocks!.Split(",").ToList();
+
+                foreach(Dictionary<string, string> stockResponse in stocksResponse)
+                {
+                    if (symbols.Contains(stockResponse["symbol"]))
+                    {
+                        Stock stock = new Stock() 
+                        {
+                            StockName = stockResponse["description"],
+                            StockSymbol = stockResponse["symbol"],
+                        };
+                    
+                        stocks.Add(stock);
+                    }
+
+                }
+            }
+
+            _logger.LogInformation("Return to ExploreView with the 25 Popular Stocks");
+            return View(stocks);
+        }
+
+        [HttpGet("stock-details/{stock}")]
+        public IActionResult Details(string stock)
+        {
+            _logger.LogInformation("User into DetailsAction in StocksController");
+            _logger.LogInformation("Return ViewComponent to UI");
+            return ViewComponent("SelectedStock", new { stockSymbolToSearch = stock });
+        }
+    }
+}
